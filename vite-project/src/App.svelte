461f718c28502d146e6dc8dabe598043d2d97dbe
Matthew Ibbetson <mmibbetson@gmail.com>
2023-06-29 18:00:56 +0200

    slowly getting some colors in place
    also added an image for later use
    will get screens of tinysteps and microcosmos
    as well

--- vite-project/src/App.svelte
+++ vite-project/src/App.svelte
@@ -1,18 +1,55 @@
 <script lang="ts">
-  import Footer from "./lib/Footer.svelte";
-  import Header from "./lib/Header.svelte";
 </script>
 
-<Header />
+<div>
+  <h1 id="title">Matthew Mark Ibbetson</h1>
+  <h2 id="subtitle">
+    <span class="textcol1">Software Engineer,</span>
+    <span class="textcol2">Musician,</span>
+    <span class="textcol3">Writer.<span class="blinker">│</span></span>
+  </h2>
 
-<p id="summary">
-  Hello! My name is Matthew and I am a passionate software engineer. I am experienced
-  in building web applications using modern technologies such as React and Express with TypeScript.
-  I am also deeply interested in the FOSS community, and am actively working on a GPL3 licensed
+  <p id="summary">
+  Hello! My name is Matthew and I am a passionate software engineer. I have experience
+  building web applications using modern technologies such as React and Express with TypeScript.
+  I am also deeply interested in the FOSS movement, and am actively working on a GPL3 licensed
   project that I look forward to sharing with the world, using Go and the Charm toolchain.
-</p>
-
-<Footer />
+  </p>
+</div>
 
 <style>
+  #title {
+    font-size: 3rem;
+    font-weight: 700;
+
+    width: 100vw;
+  }
+
+  #subtitle {
+    font-size: 2rem;
+    font-weight: 500;
+    margin-bottom: 2rem;
+  }
+
+  .textcol1 {
+    color: #8ed6a6;
+  }
+
+  .textcol2 {
+    color: #A68ED6;
+  }
+
+  .textcol3 {
+    color: #D6A68E;
+  }
+
+  .blinker {
+    animation: blink 1s step-end infinite;
+  }
+
+  @keyframes blink {
+    50% {
+      opacity: 0;
+    }
+  }
 </style>
