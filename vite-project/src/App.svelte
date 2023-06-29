ea4ecb0e4b976e3d613aaded1c69e860e95e4347
Matthew Ibbetson <mmibbetson@gmail.com>
2023-06-29 18:42:07 +0200

    putting some links in place, some layout changes

--- vite-project/src/App.svelte
+++ vite-project/src/App.svelte
@@ -1,45 +1,44 @@
 <script lang="ts">
 </script>
 
-<div>
-  <h1 id="title">Matthew Mark Ibbetson</h1>
-  <h2 id="subtitle">
-    <span class="textcol1">Software Engineer,</span>
-    <span class="textcol2">Musician,</span>
-    <span class="textcol3">Writer.<span class="blinker">│</span></span>
-  </h2>
+<header>
+  <h1 id="title">Matthew Mark Ibbetson ›
+    <span class="text-green">Software Engineer,</span>
+    <span class="text-purple">Musician,</span>
+    <span class="text-pink">Creator.<span class="blinker">│</span></span>
+  </h1>
+</header>
 
-  <p id="summary">
-  Hello! My name is Matthew and I am a passionate software engineer. I have experience
-  building web applications using modern technologies such as React and Express with TypeScript.
-  I am also deeply interested in the FOSS movement, and am actively working on a GPL3 licensed
-  project that I look forward to sharing with the world, using Go and the Charm toolchain.
-  </p>
-</div>
+<p id="summary">
+Hello! My name is Matthew and I am a passionate software engineer. I have experience
+building web applications using modern technologies such as React and Express with TypeScript.
+I am also deeply interested in the FOSS movement, and am actively working on a GPL3 licensed
+project that I look forward to sharing with the world, using Go and the Charm toolchain.
+</p>
+
+<footer>
+  <h2>Get In Touch:</h2>
+  <ul>
+    <li><a class="text-green" href="mailto:mmibbetson@gmail.com">Email</a></li>
+    <li><a class="text-purple" href="https://github.com/mmibbetson" target="_blank">GitHub</a></li>
+    <li><a class="text-pink" href="https://www.linkedin.com/in/matthew-ibbetson-42b55b260/" target="_blank">LinkedIn</a></li>
+  </ul>
+</footer>
 
 <style>
   #title {
-    font-size: 3rem;
-    font-weight: 700;
-
     width: 100vw;
   }
 
-  #subtitle {
-    font-size: 2rem;
-    font-weight: 500;
-    margin-bottom: 2rem;
-  }
-
-  .textcol1 {
+  .text-green {
     color: #8ed6a6;
   }
 
-  .textcol2 {
+  .text-purple {
     color: #A68ED6;
   }
 
-  .textcol3 {
+  .text-pink {
     color: #D6A68E;
   }
 
