33c2bf62e5fe4aba9aa321184c34a837e17d945c
Matthew Ibbetson <mmibbetson@gmail.com>
2023-06-30 02:33:51 +0200

    making social links buttons, colorizing, so on

--- vite-project/src/App.svelte
+++ vite-project/src/App.svelte
@@ -5,19 +5,21 @@
   <h1 id="title">Matthew Mark Ibbetson ›
     <span class="text-green">Software Engineer,</span>
     <span class="text-purple">Musician,</span>
-    <span class="text-pink">Creator.<span class="blinker">│</span></span>
+    <span class="text-pink">Creator.<span class="blinker">&nbsp;│</span></span>
   </h1>
 </header>
+
+<img src="../profile.webp" alt="My profile picture" id="profile-image"/>
 
 <p id="summary">
   Hello! My name is Matthew and I am a passionate software engineer. I have experience
   building web applications using modern technologies such as React and Express with TypeScript.
-  I am also deeply interested in the FOSS movement, and am actively working on a GPL3 licensed
-  project that I look forward to sharing with the world, using Go and the Charm toolchain.
+  I am also deeply interested in the future of web technologies and am actively learning and developing
+  with Rust.
 </p>
 <br/>
 <p>
-  Outside of software, I am a composer and producer of ambient, glitch, and experimental music.
+  Outside of software, I also compose and produce ambient, glitch, and experimental music.
   I have extended my sound design skills to the world of game development, and have worked on
   multiple game jam projects, as well as an independent Sci-fi animation project.
 </p>
@@ -25,15 +27,45 @@
 <footer>
   <h2>Get In Touch:</h2>
   <ul>
-    <li><a class="text-green" href="mailto:mmibbetson@gmail.com">Email</a></li>
-    <li><a class="text-purple" href="https://github.com/mmibbetson" target="_blank">GitHub</a></li>
-    <li><a class="text-pink" href="https://www.linkedin.com/in/matthew-ibbetson-42b55b260/" target="_blank">LinkedIn</a></li>
+    <li>
+      <button class="bg-green">
+        <a href="mailto:mmibbetson@gmail.com" target="_blank">
+          Email
+        </a>
+      </button>
+    </li>
+      <li>
+      <button class="bg-purple">
+        <a href="https://github.com/mmibbetson" target="_blank">
+          GitHub
+        </a>
+      </button>
+    </li>
+      <li>
+      <button class="bg-pink">
+        <a href="https://www.linkedin.com/in/matthew-ibbetson-42b55b260" target="_blank">
+          LinkedIn
+        </a>
+      </button>
+    </li>
   </ul>
 </footer>
 
 <style>
   #title {
     width: 100vw;
+  }
+
+  .bg-green {
+    background-color: #8ed6a6;
+  }
+
+  .bg-purple {
+    background-color: #A68ED6;
+  }
+
+  .bg-pink {
+    background-color: #D6A68E;
   }
 
   .text-green {
@@ -50,6 +82,11 @@
 
   .blinker {
     animation: blink 1s step-end infinite;
+  }
+
+  #profile-image {
+    padding: 0.5em;
+    width: 20em;
   }
 
   @keyframes blink {
